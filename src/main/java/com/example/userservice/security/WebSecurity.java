058e7087e529dbee1f787157a1ec89b428f64e88
jungsikyeo <mdotdom12@naver.com>
2021-05-31 08:17:15 +0900

    feign 추가

--- src/main/java/com/example/userservice/security/WebSecurity.java
+++ src/main/java/com/example/userservice/security/WebSecurity.java
@@ -25,11 +25,12 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+//        http.authorizeRequests().antMatchers("/**").permitAll();
         http.authorizeRequests().antMatchers("/actuator/**").permitAll();
-//        http.authorizeRequests().antMatchers("/**")
-//            .hasIpAddress("192.168.0.9")
-//            .and()
-//            .addFilter(getAuthenticationFilter());
+        http.authorizeRequests().antMatchers("/**")
+            .hasIpAddress("192.168.0.9")
+            .and()
+            .addFilter(getAuthenticationFilter());
 
         http.headers().frameOptions().disable();
     }
